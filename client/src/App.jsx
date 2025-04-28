533258a351442e7f746156477b45c7cc5633f97b
PuggyPotato <qingyaurou@gmail.com>
2025-04-28 22:05:23 +0800

    Added dialogue box

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,14 +9,35 @@ function App(){
   const [userInput,setUserInput] = useState("");
   const [timeCount,setTimeCount] = useState(0);
 
+  const [userTurn,setUserTurn] = useState(true);
+  
+
+  function continueDialogue(event){
+    setUserTurn(true);
+  }
+
+  function submitInstruction(){
+    setUserInput("")
+    setUserTurn(false);
+  }
 
 
 
   return(
     <>
       <div className="">
-          <div id="dialogueBox" className="border-2 w-[80%]">
+          <div id="dialogueBox" className="border-2 w-[85%] h-[30%] absolute bottom-[10%] left-[8%] rounded-lg">
+              <textarea className={`border-2 w-[100%] h-[100%] rounded-lg px-1 text-xl ${userTurn ? "" : "pointer-events-none"}`} readOnly={userTurn ? false:true}
+                        value={userInput} onChange={(e) =>setUserInput(e.target.value)}>
 
+              </textarea>
+              <button 
+              className={`border-2 absolute bottom-2 right-2 ${userTurn ? "" : "hidden"} cursor-pointer`}
+              onClick={submitInstruction}>Enter</button>
+              <textarea readOnly id="dialogue pointer-events-none"></textarea>
+              <button 
+              className={`border-2 absolute bottom-2 right-2 ${userTurn ? "hidden" : ""} cursor-pointer`}
+              onClick={continueDialogue}>Click to Continue</button>
           </div>
       </div>
     </>
