29a879c61329c25eeb24ae33a6972dc0c1204c6d
PuggyPotato <qingyaurou@gmail.com>
2025-04-30 23:45:02 +0800

    Added A Timer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,7 @@ function App(){
                            "Narrator:Every Random Command You Give Makes Him Starve More",
                            "Narrator:Remember....Provide Accurate Instructions..."]
   const [dialogue,setDialogue] = useState(openingDialogue[0]);
+  const [timeLeft,setTimeLeft] = useState("60s");
   
 
   function continueDialogue(event){
@@ -30,6 +31,7 @@ function App(){
         setAfterOpeningDialogue(true);
         setDialogue("");
         setUserTurn(true);
+        startTimer();
       }
     }
   }
@@ -39,11 +41,20 @@ function App(){
     setUserTurn(false);
   }
 
-  /*for(let i = 0;i <=totalTime;i++){
-    setInterval(() => {
-      console.log(i)
-    }, 1000);
-  }*/
+
+  function startTimer(){
+    let i = 0;
+     const interval = setInterval(() => {
+        i++
+        console.log(i)
+        setTimeLeft(60 - i + "s")
+        if(i == totalTime){
+          clearInterval(interval);
+          alert("Steve Starved To Death!");
+        }
+     }, 1000);
+  
+  }
 
 
 
@@ -51,7 +62,7 @@ function App(){
     <>
       <div className="">
           <div id="dialogueBox" className="border-2 w-[85%] h-[30%] absolute bottom-[10%] left-[8%] rounded-lg">
-              <textarea className={`border-2 w-[100%] h-[100%] rounded-lg px-1 text-2xl px-2 overflow-y-hidden ${userTurn ? "" : "pointer-events-none"}`} readOnly={userTurn ? false:true}
+              <textarea className={`border-2 w-[100%] h-[100%] rounded-lg px-1 text-2xl px-2 overflow-y-hidden ${userTurn ? "" : "pointer-events-none hidden"}`} readOnly={userTurn ? false:true}
                         value={userInput} onChange={(e) =>setUserInput(e.target.value)}
                         placeholder="Enter Your Instructions..."
                         maxLength={300}>
@@ -60,10 +71,14 @@ function App(){
               <button 
               className={`border-2 absolute bottom-2 right-2 ${userTurn ? "" : "hidden"} cursor-pointer`}
               onClick={submitInstruction}>Enter</button>
-              <textarea readOnly id="dialogue" className=" pointer-events-none absolute border-2 bottom-[0%] left-[0%] w-[100%] h-[100%]" value={dialogue}></textarea>
+              <div readOnly id="dialogue" className="typing-effect pointer-events-none absolute border-2 bottom-[0%] left-[0%] w-[100%] h-[100%] text-2xl px-2 py-1">
+                {dialogue}</div>
               <button 
               className={`border-2 absolute bottom-2 right-2 ${userTurn ? "hidden" : ""} cursor-pointer`}
               onClick={continueDialogue}>Click to Continue</button>
+          </div>
+          <div className="border-2 h-30 w-30 rounded-full absolute top-10 right-20">
+              <p className="absolute top-32 right-10 text-xl">{timeLeft}</p>
           </div>
       </div>
     </>
